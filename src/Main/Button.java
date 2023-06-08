e210379fe74f6f1e667d22146e7ce1f4b1ed424e
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-06-08 22:39:26 +0800

    style: make icon and text center

--- src/Main/Button.java
+++ src/Main/Button.java
@@ -1,15 +1,22 @@
 package Main;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Button extends JLabel {
 
     Button() {
         setOpaque(true);
         setBorder(BorderFactory.createLineBorder(Color.black));
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setVerticalAlignment(SwingConstants.CENTER);
+        setHorizontalTextPosition(JLabel.CENTER);
+        setVerticalTextPosition(JLabel.BOTTOM);
         setWhite();
     }
 
